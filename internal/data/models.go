11e48609473c3f62ba8dcf6485ef45eb7ad2bea6
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2022-04-30 22:35:41 +0530

    Setting up the Permissions Model, Checking Permissions for movies resource, Granting movies:read Permission on registration

--- internal/data/models.go
+++ internal/data/models.go
@@ -14,17 +14,19 @@ var (
 
 // Create a Models struct which wraps the MovieModel.
 type Models struct {
-	Movies MovieModel
-	Users  UserModel
-	Tokens TokenModel
+	Movies      MovieModel
+	Permissions PermissionModel
+	Users       UserModel
+	Tokens      TokenModel
 }
 
 // For ease of use, we also add a New() method which returns a Models struct containing
 // the initialized MovieModel.
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Movies: MovieModel{DB: db},
-		Users:  UserModel{DB: db},
-		Tokens: TokenModel{DB: db},
+		Movies:      MovieModel{DB: db},
+		Permissions: PermissionModel{DB: db},
+		Users:       UserModel{DB: db},
+		Tokens:      TokenModel{DB: db},
 	}
 }
