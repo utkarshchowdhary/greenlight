9c6b74dad353fbcd57eaadc065d7231623b4ff7c
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2022-05-05 21:18:10 +0530

    add endpoint to generate Additional Activation Tokens

--- cmd/api/tokens.go
+++ cmd/api/tokens.go
@@ -147,3 +147,74 @@ func (app *application) createPasswordResetTokenHandler(w http.ResponseWriter, r
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+func (app *application) createActivationTokenHandler(w http.ResponseWriter, r *http.Request) {
+	// Declare an input struct to hold the expected data from the client.
+	var input struct {
+		Email string `json:"email"`
+	}
+
+	// Parse the user's email address from the request body.
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+
+	// Validate the email provided by the client.
+	if data.ValidateEmail(v, input.Email); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	// Try to retrieve the corresponding user record for the email address. If it can't
+	// be found, return an error message to the client.
+	user, err := app.models.Users.GetByEmail(input.Email)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			v.AddError("email", "no matching email address found")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	// Return an error if the user has already been activated.
+	if user.Activated {
+		v.AddError("email", "user has already been activated")
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	// Otherwise, create a new activation token.
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	// Email the user with their activation token.
+	app.background(func() {
+		data := map[string]interface{}{
+			"activationToken": token.Plaintext,
+		}
+		// Since email addresses MAY be case sensitive, we are sending this
+		// email using the address stored in our database for the user not to the
+		// input.Email address provided by the client in this request.
+		err = app.mailer.Send(user.Email, "token_activation.tmpl", data)
+		if err != nil {
+			app.logger.PrintError(err, nil)
+		}
+	})
+
+	// Send a 202 Accepted response and confirmation message to the client.
+	env := envelope{"message": "an email will be sent to you containing activation instructions"}
+	err = app.writeJSON(w, http.StatusAccepted, env, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
